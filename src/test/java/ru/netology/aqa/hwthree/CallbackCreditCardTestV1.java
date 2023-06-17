8d1d2d606e2bf2e04261e8790dcaf8e8dbf2183a
pachimar1 <mihail.lebedev88@gmail.com>
2023-06-17 18:48:35 +0600

    update chrome driver

---
+++ src/test/java/ru/netology/aqa/hwthree/CallbackCreditCardTestV1.java
@@ -0,0 +1,56 @@
+package ru.netology.aqa.hwthree;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CallbackCreditCardTestV1 {
+    private WebDriver driver;
+
+    @BeforeAll
+//    static void setUpAll() {System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe"); }
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() {
+        driver.findElement(By.cssSelector("[data-test-id='name']")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone']")).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+
+        assertEquals(expected, actual);
+    }
+
+}
+
+
