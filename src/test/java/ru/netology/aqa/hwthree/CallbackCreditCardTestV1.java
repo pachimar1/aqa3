b44accc6d6ab0bf9c8c1d640c953ecabe5bb7f49
pachimar1 <mihail.lebedev88@gmail.com>
2023-06-17 19:09:44 +0600

    change build 2

--- src/test/java/ru/netology/aqa/hwthree/CallbackCreditCardTestV1.java
+++ src/test/java/ru/netology/aqa/hwthree/CallbackCreditCardTestV1.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CallbackCreditCardTestV1 {
